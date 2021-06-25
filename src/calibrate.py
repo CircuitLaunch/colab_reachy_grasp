de6640fbebd5b5165bce78a0d74ee207d96a942b
tigakub <tigakub@mac.com>
2021-06-25 15:35:38 -0700

    Calibration: Changed code to do just one side at a time.

--- src/calibrate.py
+++ src/calibrate.py
@@ -10,8 +10,6 @@ import time
 
 class Calibrator:
     def __init__(self):
-        moveit_commander.roscpp_initialize(sys.argv)
-        rospy.init_node("calibrate");
         robot = moveit_commander.RobotCommander()
         right_group = moveit_commander.MoveGroupCommander('right_arm')
         left_group = moveit_commander.MoveGroupCommander('left_arm')
@@ -75,73 +73,74 @@ class Calibrator:
     def setTelemetry(self, telem):
         passs
 
-    def createMaps(self):
+    def createMaps(self, side):
         rightTagSub = rospy.Subscriber(self.rightTagName, Pose, self.setRightAprilTagPose)
         leftTagSub = rospy.Subscriber(self.leftTagName, Pose, self.setLeftAprilTagPose)
         telemSub = rospy.Subscriber('dxl_telemetry', Telemetry, self.setTelemetry)
 
-        rightIds = [10, 11, 12, 13, 14, 15, 16, 17]
-        leftIds = [20, 21, 22, 23, 24, 25, 26, 27]
+
         reachyRelax = rospy.ServiceProxy('relax', Relax)
         reachyRecover = rospy.ServiceProxy('recover', Recover)
 
+        dxl_ids = {
+            'right': [10, 11, 12, 13, 14, 15, 16, 17],
+            'left': [20, 21, 22, 23, 24, 25, 26, 27]
+        }
         groups = {'right': self.right_group, 'left': self.left_group }
-        for side in ['right', 'left']:
-            rospy.loginfo(f'Calibrarting {side} side')
-            group = groups[side]
-            currentIds = rightIds if side == 'right' else leftIds
-            self.current_group = group
-            rsub = rospy.Subscriber(self.rightTagTopic, Pose, setRightAprilTagPose)
-            lsub = rospy.Subscriber(self.leftTagTopic, Pose, setLeftAprilTagPose)
-            q = quaternion_from_euler(0.0, -np.pi*0.5, 0.0)
-            map = np.array([None] * ()(self.divx+1) * (self.divy+1) * (self.divz+1)))
-            map.reshape(self.divx+1, self.divy+1, self.divz+1)
-            pose = Pos()
-            for k in range(0, self.divz + 1):
-                z = self.minz + self.divz * k
-                for j in range(0, self.divy + 1):
-                    y = self.miny + self.divy * j
-                    for i in range(0, self.divx + 1):
-                        x = self.minz + self.divx * i
-                        pose.position.x = x
-                        pose.position.y = y
-                        pose.position.z = z
-                        pose.orientation.x = q.x
-                        pose.orientation.y = q.y
-                        pose.orientation.z = q.z
-                        pose.orientation.w = q.w
-                        rospy.loginfo(f'({i}, {j}, {k}).({x}, {y}, {z}): Attempting plan and trajectory')
-                        while(True):
-                            result = self.goToPose(pose)
-                            if result == 0 or result == 1:
-                                break;
 
-                            rospy.loginfo(f'({i}, {j}, {k}).({x}, {y}, {z}): Servo error! Relaxing')
-                            # Set reachy into compliance mode
-                            relaxReq = RelaxRequest()
-                            relaxReq.dxl_ids = currentIds
-                            relax(relaxReq)
-                            # Delay
-                            rospy.loginfo(f'({i}, {j}, {k}).({x}, {y}, {z}): Resting')
-                            time.sleep(10.0)
-                            # Recover from errors
-                            rospy.loginfo(f'({i}, {j}, {k}).({x}, {y}, {z}): Recovering')
-                            recovReq = RecoverRequest()
-                            recovReq.dxl_ids = currentIds
-                            recover(recovRew)
-                            rospy.loginfo(f'({i}, {j}, {k}).({x}, {y}, {z}): Retrying')
-                            # Try again
+        currentIds = dxl_ids[side]
+        self.current_group = groups[side]
+        rsub = rospy.Subscriber(self.rightTagTopic, Pose, setRightAprilTagPose)
+        lsub = rospy.Subscriber(self.leftTagTopic, Pose, setLeftAprilTagPose)
+        q = quaternion_from_euler(0.0, -np.pi*0.5, 0.0)
+        map = np.array([None] * ()(self.divx+1) * (self.divy+1) * (self.divz+1)))
+        map.reshape(self.divx+1, self.divy+1, self.divz+1)
+        pose = Pos()
+        for k in range(0, self.divz + 1):
+            z = self.minz + self.divz * k
+            for j in range(0, self.divy + 1):
+                y = self.miny + self.divy * j
+                for i in range(0, self.divx + 1):
+                    x = self.minz + self.divx * i
+                    pose.position.x = x
+                    pose.position.y = y
+                    pose.position.z = z
+                    pose.orientation.x = q.x
+                    pose.orientation.y = q.y
+                    pose.orientation.z = q.z
+                    pose.orientation.w = q.w
+                    rospy.loginfo(f'({i}, {j}, {k}).({x}, {y}, {z}): Attempting plan and trajectory')
+                    while(True):
+                        result = self.goToPose(pose)
+                        if result == 0 or result == 1:
+                            break;
 
-                        # Wait for latest pose update?
-                        if result == 0:
-                            rospy.loginfo(f'({i}, {j}, {k}).({x}, {y}, {z}): getAprilTagPosition() not yet implmeneted')
-                            ax, ay, az = self.getAprilTagPosition(side)
-                            map[i, j, k] = ((x, y, z), (x - ax, y - ay, z - az))
-                        elif result == 1:
-                            rospy.loginfo(f'({i}, {j}, {k}).({x}, {y}, {z}): No plan for goal pose. Marking point outside workspace')
-                            map[i, j, k] = None
+                        rospy.loginfo(f'({i}, {j}, {k}).({x}, {y}, {z}): Servo error! Relaxing')
+                        # Set reachy into compliance mode
+                        relaxReq = RelaxRequest()
+                        relaxReq.dxl_ids = currentIds
+                        relax(relaxReq)
+                        # Delay
+                        rospy.loginfo(f'({i}, {j}, {k}).({x}, {y}, {z}): Resting')
+                        time.sleep(10.0)
+                        # Recover from errors
+                        rospy.loginfo(f'({i}, {j}, {k}).({x}, {y}, {z}): Recovering')
+                        recovReq = RecoverRequest()
+                        recovReq.dxl_ids = currentIds
+                        recover(recovRew)
+                        rospy.loginfo(f'({i}, {j}, {k}).({x}, {y}, {z}): Retrying')
+                        # Try again
 
-                        time.sleep(1.0)
+                    # Wait for latest pose update?
+                    if result == 0:
+                        rospy.loginfo(f'({i}, {j}, {k}).({x}, {y}, {z}): getAprilTagPosition() not yet implmeneted')
+                        ax, ay, az = self.getAprilTagPosition(side)
+                        map[i, j, k] = ((x, y, z), (x - ax, y - ay, z - az))
+                    elif result == 1:
+                        rospy.loginfo(f'({i}, {j}, {k}).({x}, {y}, {z}): No plan for goal pose. Marking point outside workspace')
+                        map[i, j, k] = None
+
+                    time.sleep(1.0)
         return map
 
     def goToPose(self, pose):
@@ -170,3 +169,30 @@ class Calibrator:
         group.execute(plan, wait = True)
         self.isExecuting = False
         # signal completion
+
+def main():
+    moveit_commander.roscpp_initialize(sys.argv)
+    rospy.init_node('calibrate');
+
+    side = rospy.get_param('/calibrate/side')
+    mapSavePath = rospy.get_param('/calibrate/save_file_path')
+
+    calibrator = Calibrator()
+
+    rospy.loginfo('************************************************************')
+    rospy.loginfo(f'Calibrating {side} side')
+    map = calibrate.createMap(side)
+    rospy.loginfo(f'Calibration complete for {side} side')
+
+    rospy.loginfo('------------------------------------------------------------')
+    rospy.loginfo(f'Saving map for {side} side to {mapSavePath}')
+    rospy.loginfo('------------------------------------------------------------')
+
+    f = open(mapSavePath, 'wt')
+    np.save(f, map)
+    close(f)
+    rospy.loginfo('************************************************************')
+
+if __name__ == '__main__':
+    main()
+    

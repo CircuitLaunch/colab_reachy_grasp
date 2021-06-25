4ad7996aa2d5a4a8e9e756caadb7a2131135b821
Liam Han <liamhan0905@gmail.com>
2021-06-25 14:49:37 -0700

    edit: changed to so you have to manually type which publishers to be active

--- src/apriltag_topic_pub.py
+++ src/apriltag_topic_pub.py
@@ -2,17 +2,13 @@
 import sys
 import tf
 from apriltag_ros.msg import AprilTagDetectionArray
-from geometry_msgs.msg import Pose
+from geometry_msgs.msg import PoseStamped
 import rospy
 
 def tf_handle(msgs):
 
-    # rate = rospy.Rate(10)
     for msg in msgs.detections:
 
-        topicName = "apriltag_" + str(msg.id[0])
-        apriltagPub = rospy.Publisher(topicName,Pose, queue_size=10)
-        
         # hardware
         # msg.pose.header.frame_id = "apriltag_" + str(msg.id[0])
         # simulation
@@ -33,24 +29,35 @@ def tf_handle(msgs):
         #                   "camera_link")
         #                 #   "camera_link_optical") for simluation
 
-        pose = Pose()
-        pose.position.x = msg.pose.pose.pose.position.x
-        pose.position.y = msg.pose.pose.pose.position.y
-        pose.position.z = msg.pose.pose.pose.position.z
-        pose.orientation.x = msg.pose.pose.pose.orientation.x
-        pose.orientation.y = msg.pose.pose.pose.orientation.y
-        pose.orientation.z = msg.pose.pose.pose.orientation.z
+        poseStamped = PoseStamped()
+        poseStamped.header.stamp = rospy.Time.now()
+        poseStamped.pose.position.x = msg.pose.pose.position.x
+        poseStamped.pose.position.y = msg.pose.pose.position.y
+        poseStamped.pose.position.z = msg.pose.pose.position.z
+        poseStamped.pose.orientation.x = msg.pose.pose.orientation.x
+        poseStamped.pose.orientation.y = msg.pose.pose.orientation.y
+        poseStamped.pose.orientation.z = msg.pose.pose.orientation.z
 
-        while apriltagPub.get_num_connections() <1:
-            rospy.loginfo("waiting for the publisher to open")
-        apriltagPub.publish(pose)
-        # rate.sleep()
-        
+        while cubeATPub.get_num_connections() <1 or reachyATPub.get_num_connections() <1:
+            rospy.loginfo("waiting for the publishers to be active")
+
+        if msg.id[0] == 4: # reachy chest
+            poseStamped.header.frame_id = 'reachy_chest'
+            cubeATPub.publish(poseStamped)
+    
+        elif msg.id[0] == 5: # cube
+            poseStamped.header.frame_id = 'cube'
+            reachyATPub.publish(reachyATPub)
+ 
 
 if __name__ == "__main__":  
     try:
         rospy.init_node('tag_detection_to_tf')
         rospy.Subscriber('/tag_detections',AprilTagDetectionArray,tf_handle)
+
+        # Add more publishers if needed
+        cubeATPub = rospy.Publisher('cubePose',PoseStamped, queue_size=10)
+        reachyATPub = rospy.Publisher('reachyPose', PoseStamped, queue_size=10)
 
         rospy.spin()
 

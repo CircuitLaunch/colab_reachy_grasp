bca13557224348cd9814b450cf6214e5aa8dfbf6
tigakub <tigakub@mac.com>
2021-07-04 02:20:13 -0700

    bug

--- src/apriltag_topic_pub.py
+++ src/apriltag_topic_pub.py
@@ -5,53 +5,42 @@ from apriltag_ros.msg import AprilTagDetectionArray
 from geometry_msgs.msg import PoseStamped, Pose
 import geometry_msgs.msg
 import rospy
+import time
 
 class Apriltag_Converter():
 
     def __init__(self):
+        pass
+        # self.listener = tf2_ros.TransformListener(self.tfBuffer)
+
+    def apriltag_callback(self, msgs):
         self.tfBuffer = tf2_ros.Buffer()
-        self.listener = tf2_ros.TransformListener(self.tfBuffer)
-        self.rate = rospy.Rate(5.0)
-
-    def apriltag_callback(self,msgs):
-
+        rate = rospy.Rate(5.0)
+        
         try:
-            trans = self.tfBuffer.lookup_transform('pedestal', 'apriltag_5',rospy.Time(0))
-
-            for msg in msgs.detections:
-
-                # poseStamped = PoseStamped()
-                # poseStamped.header.stamp = rospy.Time.now()
-                # poseStamped.pose.position.x = msg.pose.pose.pose.position.x
-                # poseStamped.pose.position.y = msg.pose.pose.pose.position.y
-                # poseStamped.pose.position.z = msg.pose.pose.pose.position.z
-                # poseStamped.pose.orientation.x = msg.pose.pose.pose.orientation.x
-                # poseStamped.pose.orientation.y = msg.pose.pose.pose.orientation.y
-                # poseStamped.pose.orientation.z = msg.pose.pose.pose.orientation.z
-
-                # while cubeATPub.get_num_connections() <1 or reachyATPub.get_num_connections() <1:
-                #     rospy.loginfo("waiting for the publishers to be active")
-
-                # if msg.id[0] == 4: # reachy chest
-                #     trans = self.tfBuffer.lookup_transform('pedestal', 'apriltag_4',rospy.Time(0))
+            # rospy.loginfo("cb")
+            self.trans = self.tfBuffer.lookup_transform('pedestal', 'apriltag_5',rospy.Time(0))
+            cube_pose = geometry_msgs.msg.Pose()
+            cube_pose.position = self.trans.transform.translation
+            cube_pose.orientation = self.trans.transform.rotation
+            # trans = self.tfBuffer.lookup_transform('pedestal', 'apriltag_5',rospy.Time(0))
+            rospy.loginfo(f'wtf: {self.trans}')
+            # for msg in msgs.detections:
+           
+                # if msg.id[0] == 5: # cube
                 #     _pose = geometry_msgs.msg.Pose()
                 #     _pose.position = trans.transform.translation
                 #     _pose.orientation = trans.transform.rotation
-                #     # poseStamped.header.frame_id = 'reachy_chest'
-                #     reachyATPub.publish(poseStamped)
-            
-                if msg.id[0] == 5: # cube
-                    _pose = geometry_msgs.msg.Pose()
-                    _pose.position = trans.transform.translation
-                    _pose.orientation = trans.transform.rotation
-                    # poseStamped.header.frame_id = 'cube'
-                    cubeATPub.publish(_pose)
+                #     rospy.loginfo(_pose)
+                #     # poseStamped.header.frame_id = 'cube'
+                #     cubeATPub.publish(_pose)
 
         except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
-            self.rate.sleep()
+            rate.sleep()
 
 if __name__ == "__main__":  
     try:
+        rospy.loginfo("in main")
         apriltag_converter = Apriltag_Converter()
         rospy.init_node('apriltag_pose_pub',anonymous=True)
 
